b4a1d337dda3ae7a09cc4afde5a005efeb6660f8
Anika Hussen <ah3866@ad.nyu.edu>
2019-10-07 10:48:56 -0400

    scene changing

---
+++ Assets/Scripts/teleporting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporting : MonoBehaviour
+{
+    public Transform teleportTarget; //new spot
+    public GameObject thePlayer; //the object that is moving
+
+    private void OnTriggerEnter(Collider other)//collision
+    {
+        thePlayer.transform.position = teleportTarget.transform.position; //translate (equate the location to new location)
+    }
+}
